3ea65dd0e6b8afa5bd6ace82c53b896797b626d6
KWAKMANBO <kwak98hs@gmail.com>
2023-07-27 01:23:49 +0900

    009 failed version

---
+++ Problem010_MYversion.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import java.util.StringTokenizer;
+import java.util.Arrays;
+
+import java.io.IOException;
+
+// 실패한 버전
+public class Problem010_MYversion {
+   static int Arr[];
+   static int SubArr[];
+
+    public static void main(String args[]) throws IOException{
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int A = Integer.parseInt(st.nextToken()); 
+
+        Arr = new int[N];
+        SubArr = new int[N];
+
+        st = new StringTokenizer(bf.readLine());
+
+        // 수열 전체를 입력 
+        for(int i = 0; i < N; i++){
+            Arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int  min = Arr[0];
+
+        for(int i = 0; i < A; i++){
+            SubArr[i] = Arr[i] ;
+            if(SubArr[i] < min){
+                min = SubArr[i];
+            }
+            System.out.println(min);
+        
+        }
+
+       for(int i = A  ; i < N; i++){
+             int sub[] = new int[A];
+            for(int  j = i - A; j < i; j++ ){
+               
+                for(int k = 0; k< A; k++){
+                
+                if(j+k >= N)break;
+                    sub[k] = Arr[j + k ];  
+                }
+                Arrays.sort(sub);
+               
+            }
+            System.out.println(sub[0]);
+        }
+   }
+}
