69eb87e50f90591cebba934c912d46ae3d28b751
KWAKMANBO <kwak98hs@gmail.com>
2023-08-10 00:47:15 +0900

    add prob13

--- Problem014_MYversion.java
+++ Problem014_MYversion.java
@@ -13,9 +13,14 @@ public class Problem014_MYversion {
         Comparator<Integer> cp = new Comparator<Integer>(){
             @Override
             public int compare(Integer o1, Integer o2){
-                //return Math.abs(o1) < Math.abs(o2) ? -1:1;
-                return o1 < o2 ? -1:1;
-
+                if(Math.abs(o1) < Math.abs(o2)){ // o1이 o2보다 작다면
+                    return -1;
+                }else if(Math.abs(o1) == Math.abs(o2)){
+                    return o1 < o2 ? -1:1;
+                }else{
+                    return 1;
+                }
+            
                 // o1의 절댓값이 o2보다 작다면 1을반환, --> 1 순서를 바꿈
                 // o1의 절댓값이 o2보다 크다면 -1을반환 --> -1 순서를 안바꿈
             }
@@ -27,8 +32,6 @@ public class Problem014_MYversion {
         for(int i = 0; i < n ; i++){
             int x = Integer.parseInt(br.readLine());
 
-            
-
             if(x == 0 ){
                 if(pq.isEmpty()){
                     bw.write("0\n");
@@ -39,7 +42,6 @@ public class Problem014_MYversion {
                 pq.add(x);
             }
         }
-        System.out.println();
        bw.flush();
     }
 }
