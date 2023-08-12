87f4ad3a6e6c8038a594aaa3773430f5026496a4
KWAKMANBO <kwak98hs@gmail.com>
2023-08-13 03:57:25 +0900

    add

--- Problem014_MYversion.java
+++ Problem014_MYversion.java
@@ -2,7 +2,6 @@ import java.util.*;
 import java.io.*;
 
 public class Problem014_MYversion {
-    
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw =new BufferedWriter(new OutputStreamWriter(System.out));
--- SelectionSort.java
+++ SelectionSort.java
@@ -2,8 +2,10 @@ public class SelectionSort {
     public static void main(String[] args){
         
         int[] a = new int[10];
-
         
-
+        for( int i = 0; i <10; i++){
+            a[i] = (int)((Math.random()*100) +1);
+            
+        }
     }
 }
